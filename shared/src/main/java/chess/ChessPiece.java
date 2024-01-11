6159eee8813ba6f88c93a29cd37fce4c7b270bd9
James Wade <note2jwade@gmail.com>
2024-01-10 22:54:46 -0700

    Rook Test Working

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -168,8 +168,74 @@ public class ChessPiece {
 
     private Collection<ChessMove> rookMoves(ChessBoard board, ChessPosition myPosition) {
         HashSet<ChessMove> moves = new HashSet<ChessMove>();
-        int row = myPosition.getRow();
-        int col = myPosition.getColumn();
+        int myRow = myPosition.getRow();
+        int myCol = myPosition.getColumn();
+
+        int row = myRow;
+        int col = myCol;
+
+        //Going up
+        outer:
+        while (row <= 8) {
+            row++;
+            if (row > 8) {
+                break outer;
+            } //break if coordinates are out of bounds
+            ChessPosition newPosition = new ChessPosition(row,col);
+            ChessPiece newPositionPiece = board.getPiece(newPosition);
+            if (!boolValidPosition(moves, board.getPiece(myPosition), newPositionPiece, myPosition, newPosition, null)) {
+                break outer;
+            }
+        }
+
+        row = myRow;
+
+        //Going down
+        outer:
+        while (row >= 1) {
+            row--;
+            if (row < 1) {
+                break outer;
+            } //break if coordinates are out of bounds
+            ChessPosition newPosition = new ChessPosition(row,col);
+            ChessPiece newPositionPiece = board.getPiece(newPosition);
+            if (!boolValidPosition(moves, board.getPiece(myPosition), newPositionPiece, myPosition, newPosition, null)) {
+                break outer;
+            }
+        }
+
+        row = myRow;
+
+        //Going left
+        outer:
+        while (col >= 1) {
+            col--;
+            if (col < 1) {
+                break outer;
+            } //break if coordinates are out of bounds
+            ChessPosition newPosition = new ChessPosition(row,col);
+            ChessPiece newPositionPiece = board.getPiece(newPosition);
+            if (!boolValidPosition(moves, board.getPiece(myPosition), newPositionPiece, myPosition, newPosition, null)) {
+                break outer;
+            }
+        }
+
+        col = myCol;
+
+        //Going right
+        outer:
+        while (col <= 8) {
+            col++;
+            if (col > 8) {
+                break outer;
+            } //break if coordinates are out of bounds
+            ChessPosition newPosition = new ChessPosition(row,col);
+            ChessPiece newPositionPiece = board.getPiece(newPosition);
+            if (!boolValidPosition(moves, board.getPiece(myPosition), newPositionPiece, myPosition, newPosition, null)) {
+                break outer;
+            }
+        }
+
         return moves;
     }
 
