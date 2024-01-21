b075fc9cae5a5708c9807668cb66e2c19b848bb3
James Wade <note2jwade@gmail.com>
2024-01-20 19:06:42 -0700

    Make changes

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -86,32 +86,4 @@ public class ChessPiece {
         return moves;
     }
 
-    private boolean boolValidPosition(HashSet<ChessMove> moves, ChessPiece myPositionPiece, ChessPiece newPositionPiece, ChessPosition myPosition, ChessPosition newPosition, PieceType promotionPiece) {
-        if (newPositionPiece == null) {
-            moves.add(new ChessMove(myPosition, newPosition, promotionPiece));
-            return true;
-        }
-        else {
-            if (newPositionPiece.getTeamColor() == myPositionPiece.getTeamColor()) { return false; }
-            else if (newPositionPiece.getTeamColor() != myPositionPiece.getTeamColor()) {
-                moves.add(new ChessMove(myPosition, newPosition, promotionPiece));
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private void validPosition(HashSet<ChessMove> moves, ChessPiece myPositionPiece, ChessPiece newPositionPiece, ChessPosition myPosition, ChessPosition newPosition, PieceType promotionPiece) {
-        if (newPositionPiece == null) {
-            moves.add(new ChessMove(myPosition, newPosition, promotionPiece));
-            return;
-        } else {
-            if (newPositionPiece.getTeamColor() == myPositionPiece.getTeamColor()) {
-                return;
-            } else if (newPositionPiece.getTeamColor() != myPositionPiece.getTeamColor()) {
-                moves.add(new ChessMove(myPosition, newPosition, promotionPiece));
-                return;
-            }
-        }
-    }
 }
