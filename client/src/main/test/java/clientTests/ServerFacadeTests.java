94f0c8f17fe218942af27c5d67c6121eef477e45
James Wade <note2jwade@gmail.com>
2024-03-18 15:25:35 -0600

    Phase 5 - Finished unit tests

--- client/src/main/test/java/clientTests/ServerFacadeTests.java
+++ client/src/main/test/java/clientTests/ServerFacadeTests.java
@@ -1,8 +1,15 @@
 package clientTests;
 
 import ServerFacade.ServerFacade;
+import model.GameData;
+import model.UserData;
 import org.junit.jupiter.api.*;
+import responseException.ResponseException;
+import responseGames.GameResponseClass;
+import responseGames.PlayerJoinRequest;
 import server.Server;
+
+import java.util.HashSet;
 
 
 public class ServerFacadeTests {
@@ -32,36 +39,145 @@ public class ServerFacadeTests {
 
     @Test
     void registerUserSuccess() {
-
+        Assertions.assertDoesNotThrow(() -> facade.registerUser(new UserData("user1", "pass1", "email")));
     }
 
     @Test
     void registerUserFail() {
-
+        Assertions.assertDoesNotThrow(() -> facade.registerUser(new UserData("user1", "pass1", "email")));
+        Assertions.assertThrows(ResponseException.class, () -> facade.registerUser(new UserData("user1", "pass1", "email")));
     }
 
     @Test
-    void loginUser() {
+    void loginUserSuccess() {
+        Assertions.assertDoesNotThrow(() -> facade.registerUser(new UserData("user1", "pass1", "email")));
+        Assertions.assertDoesNotThrow(() -> facade.loginUser(new UserData("user1", "pass1", null)));
     }
 
     @Test
-    void logoutUser() {
+    void loginUserFail() {
+        Assertions.assertThrows(ResponseException.class, () -> facade.loginUser(new UserData("user1", "pass1", null)));
     }
 
     @Test
-    void createGame() {
+    void logoutUserSuccess() {
+        Assertions.assertDoesNotThrow(() -> facade.registerUser(new UserData("user1", "pass1", "email")));
+        Assertions.assertDoesNotThrow(() -> facade.loginUser(new UserData("user1", "pass1", null)));
+        Assertions.assertDoesNotThrow(() -> facade.logoutUser());
     }
 
     @Test
-    void listGames() {
+    void logoutUserFail() {
+        Assertions.assertDoesNotThrow(() -> facade.registerUser(new UserData("user1", "pass1", "email")));
+        Assertions.assertThrows(ResponseException.class, () -> facade.logoutUser());
+
+        Assertions.assertDoesNotThrow(() -> facade.loginUser(new UserData("user1", "pass1", null)));
+        Assertions.assertDoesNotThrow(() -> facade.logoutUser());
+        Assertions.assertThrows(ResponseException.class, () -> facade.logoutUser());
     }
 
     @Test
-    void joinGame() {
+    void createGameSuccess() {
+        Assertions.assertDoesNotThrow(() -> facade.registerUser(new UserData("user1", "pass1", "email")));
+        Assertions.assertDoesNotThrow(() -> facade.loginUser(new UserData("user1", "pass1", null)));
+        Assertions.assertDoesNotThrow(() -> facade.createGame(new GameData(-1, null, null, "game1", null)));
+    }
+
+    @Test
+    void createGameFail() {
+        Assertions.assertDoesNotThrow(() -> facade.registerUser(new UserData("user1", "pass1", "email")));
+        Assertions.assertDoesNotThrow(() -> facade.loginUser(new UserData("user1", "pass1", null)));
+        Assertions.assertDoesNotThrow(() -> facade.createGame(new GameData(-1, null, null, "game1", null)));
+        Assertions.assertThrows(ResponseException.class, () -> facade.createGame(new GameData(-1, null, null, "game1", null)));
+    }
+
+    @Test
+    void listGamesSuccess() {
+        Assertions.assertDoesNotThrow(() -> facade.registerUser(new UserData("user1", "pass1", "email")));
+        Assertions.assertDoesNotThrow(() -> facade.loginUser(new UserData("user1", "pass1", null)));
+        Assertions.assertDoesNotThrow(() -> facade.createGame(new GameData(-1, null, null, "game1", null)));
+        try {
+            HashSet<GameResponseClass.GameList> games = facade.listGames().getGames();
+            for (var game : games) {
+                Assertions.assertNull(game.blackUsername());
+                Assertions.assertNull(game.whiteUsername());
+                Assertions.assertTrue(game.gameID() > 0, "gameID is not default value");
+                Assertions.assertEquals("game1", game.gameName());
+            }
+        } catch (ResponseException ex) {
+            Assertions.fail("Expected no error but got " + ex.getMessage());
+        }
+    }
+
+    @Test
+    void listGamesFail() {
+        Assertions.assertDoesNotThrow(() -> facade.registerUser(new UserData("user1", "pass1", "email")));
+        Assertions.assertDoesNotThrow(() -> facade.loginUser(new UserData("user1", "pass1", null)));
+        Assertions.assertDoesNotThrow(() -> facade.createGame(new GameData(-1, null, null, "game1", null)));
+        try {
+            HashSet<GameResponseClass.GameList> games = facade.listGames().getGames();
+            for (var game : games) {
+                Assertions.assertNull(game.blackUsername());
+                Assertions.assertNull(game.whiteUsername());
+                Assertions.assertTrue(game.gameID() > 0, "gameID is not default value");
+                Assertions.assertNotEquals("game2", game.gameName());
+            }
+        } catch (ResponseException ex) {
+            Assertions.fail("Expected no error but got " + ex.getMessage());
+        }
+    }
+
+    @Test
+    void joinGameSuccess() {
+        Assertions.assertDoesNotThrow(() -> facade.registerUser(new UserData("user1", "pass1", "email")));
+        Assertions.assertDoesNotThrow(() -> facade.loginUser(new UserData("user1", "pass1", null)));
+        Assertions.assertDoesNotThrow(() -> facade.createGame(new GameData(-1, null, null, "game1", null)));
+        try {
+            HashSet<GameResponseClass.GameList> games = facade.listGames().getGames();
+            int gameID = 0;
+            for (var game : games) {
+                gameID = game.gameID();
+            }
+
+            int finalGameID = gameID;
+            Assertions.assertDoesNotThrow(() -> facade.joinGame(new PlayerJoinRequest("WHITE", finalGameID)));
+            games = facade.listGames().getGames();
+            for (var game : games) {
+                Assertions.assertEquals("user1", game.whiteUsername());
+            }
+
+            Assertions.assertDoesNotThrow(() -> facade.joinGame(new PlayerJoinRequest(null, finalGameID)));
+        } catch (ResponseException ex) {
+            Assertions.fail("Expected no error but got " + ex.getMessage());
+        }
+    }
+
+    @Test
+    void joinGameFail() {
+        Assertions.assertDoesNotThrow(() -> facade.registerUser(new UserData("user1", "pass1", "email")));
+        Assertions.assertDoesNotThrow(() -> facade.loginUser(new UserData("user1", "pass1", null)));
+        Assertions.assertDoesNotThrow(() -> facade.createGame(new GameData(-1, null, null, "game1", null)));
+        try {
+            HashSet<GameResponseClass.GameList> games = facade.listGames().getGames();
+            int gameID = 0;
+            for (var game : games) {
+                gameID = game.gameID();
+            }
+
+            int finalGameID = gameID;
+            Assertions.assertDoesNotThrow(() -> facade.joinGame(new PlayerJoinRequest("WHITE", finalGameID)));
+            Assertions.assertThrows(ResponseException.class, () -> facade.joinGame(new PlayerJoinRequest("WHITE", finalGameID)));
+        } catch (ResponseException ex) {
+            Assertions.fail("Expected no error but got " + ex.getMessage());
+        }
     }
 
     @Test
     void delete() {
+        Assertions.assertDoesNotThrow(() -> facade.registerUser(new UserData("user1", "pass1", "email")));
+        Assertions.assertDoesNotThrow(() -> facade.loginUser(new UserData("user1", "pass1", null)));
+        Assertions.assertDoesNotThrow(() -> facade.createGame(new GameData(-1, null, null, "game1", null)));
+        Assertions.assertDoesNotThrow(() -> facade.delete());
     }
 
 }
--- shared/src/main/java/responseGames/GameResponseClass.java
+++ shared/src/main/java/responseGames/GameResponseClass.java
@@ -38,4 +38,6 @@ public class GameResponseClass {
 
         return output.toString();
     }
+
+    public HashSet<GameList> getGames() { return games; }
 }
