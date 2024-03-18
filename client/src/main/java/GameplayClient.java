f36db80d5e6bd506ccd346c67a6e467776c7c5f3
James Wade <note2jwade@gmail.com>
2024-03-18 11:27:18 -0600

    Phase 5 - Basic chessboard printout

--- client/src/main/java/GameplayClient.java
+++ client/src/main/java/GameplayClient.java
@@ -1,4 +1,5 @@
 import chess.ChessBoard;
+import ui.EscapeSequences;
 
 public class GameplayClient {
 
@@ -12,6 +13,55 @@ public class GameplayClient {
         ChessBoard board = new ChessBoard();
         board.resetBoard();
         String boardStr = board.toString();
-        return board.toString();
+        var rows = boardStr.split("\n");
+
+        StringBuilder output = new StringBuilder();
+        output.append(EscapeSequences.SET_TEXT_COLOR_LIGHT_GREY);
+        int i = 0;
+        for (var row : rows) {
+            for (var c : row.toCharArray()) {
+                switch(c) {
+                    case '|' -> output.append(EscapeSequences.BORDER);
+                    case ' ' -> output.append(EscapeSequences.EMPTY);
+                    default -> {
+                        output.append(EscapeSequences.SET_TEXT_COLOR_WHITE);
+                        output.append(parseChessCharacter(c));
+                        output.append(EscapeSequences.SET_TEXT_COLOR_LIGHT_GREY);
+                    }
+                }
+            }
+            if (i != 7) {
+                output.append("\n");
+            }
+            i++;
+        }
+
+        output.append(EscapeSequences.SET_TEXT_COLOR_WHITE);
+        return output.toString();
+    }
+
+    private String parseChessCharacter(char c) {
+        if (Character.isLowerCase(c)) {
+            return switch(c) {
+                case 'p' -> EscapeSequences.WHITE_PAWN;
+                case 'r' -> EscapeSequences.WHITE_ROOK;
+                case 'b' -> EscapeSequences.WHITE_BISHOP;
+                case 'n' -> EscapeSequences.WHITE_KNIGHT;
+                case 'k' -> EscapeSequences.WHITE_KING;
+                case 'q' -> EscapeSequences.WHITE_QUEEN;
+                default -> " ";
+            };
+        }
+        else {
+            return switch(c) {
+                case 'P' -> EscapeSequences.BLACK_PAWN;
+                case 'R' -> EscapeSequences.BLACK_ROOK;
+                case 'B' -> EscapeSequences.BLACK_BISHOP;
+                case 'N' -> EscapeSequences.BLACK_KNIGHT;
+                case 'K' -> EscapeSequences.BLACK_KING;
+                case 'Q' -> EscapeSequences.BLACK_QUEEN;
+                default -> " ";
+            };
+        }
     }
 }
